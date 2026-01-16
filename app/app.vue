1202cf46cf75a2dbe32d849c4dcabe92c7cf7e42
wizwiz <abezzi@outlook.cl>
2026-01-16 13:07:39 -0300

    feat(app): dont load the screen if isn't loaded yet

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,18 @@
 <template>
-  <NuxtLayout>
+  <div
+    v-if="authStore.isLoading"
+    class="min-h-screen flex items-center justify-center bg-gray-950"
+  >
+    <div class="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500" />
+  </div>
+
+  <NuxtLayout v-else>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+import { useAuthStore } from '../stores/auth'
+
+const authStore = useAuthStore()
+</script>
