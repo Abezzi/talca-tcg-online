734daf1d41908929bff2581f087248c1ca52f7d3
wizwiz <abezzi@outlook.cl>
2026-01-26 18:44:11 -0300

    feat(db): cards and decks schemas

--- convex/schema.ts
+++ convex/schema.ts
@@ -13,6 +13,46 @@ export default defineSchema({
 
   currencies: defineTable({
     coins: v.number(),
-    userId: v.id('users')
+    userId: v.id("users"),
+  }),
+
+  cards: defineTable({
+    name: v.string(),
+    level: v.number(),
+    type: v.union(v.literal("normal"), v.literal("trap"), v.literal("spell")),
+    atack: v.optional(v.number()),
+    defense: v.optional(v.number()),
+    rarity: v.union(
+      v.literal("n"),
+      v.literal("r"),
+      v.literal("sr"),
+      v.literal("ur"),
+    ),
+  }).index("by_name", ["name"]),
+
+  // N:M users:cards
+  user_unlocked_cards: defineTable({
+    userId: v.id("users"),
+    cardId: v.id("cards"),
   })
+    .index("by_user", ["userId"])
+    .index("by_user_card", ["userId", "cardId"])
+    .index("by_card", ["cardId"]),
+
+  // decks :: 1:N user:decks
+  decks: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+  }),
+
+  // N:M deck:cards
+  deck_card_entries: defineTable({
+    deckId: v.id("decks"),
+    cardId: v.id("cards"),
+    quantity: v.number(),
+    position: v.optional(v.number()),
+  })
+    .index("by_deck", ["deckId"])
+    .index("by_deck_card", ["deckId", "cardId"])
+    .index("by_card", ["cardId"]),
 });
